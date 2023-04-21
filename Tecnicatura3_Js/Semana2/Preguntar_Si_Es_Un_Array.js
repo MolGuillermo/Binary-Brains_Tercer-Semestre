4d88f1884825752a8679e20b0f9b8175b9a1971d
Mauricio <108999183+mauriciosandrea@users.noreply.github.com>
2023-04-21 09:34:01 -0300

    Create Preguntar_Si_Es_Un_Array.js
    preguntar si es un array

---
+++ Tecnicatura3_Js/Semana2/Preguntar_Si_Es_Un_Array.js
@@ -0,0 +1,7 @@
+const autos = ["Ferrari","renault","BMW"];
+console.log(autos);
+
+//Como preguntar si es un array o arreglo
+console.log(Array.isArray(autos));//Devuelve un booleano
+
+console.log(autos instanceof Array);//Preguntamos si la variable es una instacia de la clase Array
