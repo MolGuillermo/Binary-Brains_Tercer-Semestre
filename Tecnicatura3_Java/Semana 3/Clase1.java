0e3d3be122506c079edf7bd34a3c5f6820fa9aac
MolGuillermo <111472220+MolGuillermo@users.noreply.github.com>
2023-05-17 09:53:17 -0300

    Modificadores de acceso java

---
+++ Tecnicatura3_Java/Semana 3/Clase1.java
@@ -0,0 +1,16 @@
+
+package Paquete1;
+public class Clase1 {
+    public String atributoPublic = "Valor atributo public";
+    
+    public Clase1(){
+        System.out.println("Constructor public");
+    }
+    public void metodoPublico(){
+        System.out.println("Metodo public");
+        
+        
+        
+    }
+    
+}
